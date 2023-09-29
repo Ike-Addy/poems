4cf08cb86593b5af26a79dd1c691c9a401aea18a
Ike Addy <ikaddy05@gmail.com>
2023-09-29 04:32:59 -0400

    Added poem object Created function to push new poems to myPoems array Added submit btn and added runs addPoemToArray function on click

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-myPoems 
+myPoems = [];
+
+function Poem(title, poem) {
+    this.title
+    this. poem
+}
+
+function addPoemToArray() {
+    let title = document.querySelector('#title').value;
+    let poem = document.querySelector('#poem').value;
+
+    let newPoem = new Poem(title, poem);
+    myPoems.push(newPoem);
+}
+
+function render() {
+    
+}
+
+let submitBtn = document.querySelector('#add-poem');
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    addPoemToArray();
+    console.log(myPoems)
+})
+
+
+
